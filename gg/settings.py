f05e4465fde3fc0026eb3fa4eca581c74c82ef1f
Mateusz <goral1996@gmail.com>
2020-05-04 17:18:23 +0200

    heroku-django

--- gg/settings.py
+++ gg/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-TATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+# STATIC_URL = '/static/'
+# TATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
