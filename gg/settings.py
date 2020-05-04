e0ecab53b1778114baed1d2173d16d1565309f80
Mateusz <goral1996@gmail.com>
2020-05-04 17:07:33 +0200

    config

--- gg/settings.py
+++ gg/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.conf import settings
-import mimetypes
+import django_heroku
 
-mimetypes.add_type("text/css", ".css", True)
-
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
