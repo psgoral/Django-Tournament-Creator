ca3ec431d9f695cb81ec298263465fc2ba9b6fee
Mateusz <goral1996@gmail.com>
2020-05-04 16:57:32 +0200

    mines

--- gg/settings.py
+++ gg/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.conf import settings
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
